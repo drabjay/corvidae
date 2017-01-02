dd08a8c2e7793337364d8dd03adf0d9224784568
John Bard <johnbard@globalnet.co.uk>
2017-01-02 14:43:34 +0000

    Refactor tests

--- spec/packages/apis/varchar2_api_spec.rb
+++ spec/packages/apis/varchar2_api_spec.rb
@@ -1,18 +1,21 @@
 require 'spec_helper'
 
 describe 'varchar2_api' do
+  let(:nsv) { '$$$Null$$$' }
+
   describe '.nsv' do
     subject { plsql.varchar2_api.nsv }
-    it { is_expected.to eq '$$$Null$$$' }
+    it { is_expected.to eq nsv }
   end
 
   describe '.is_nsv' do
     context 'when passed nsv' do
-      subject { plsql.varchar2_api.is_nsv('$$$Null$$$') }
+      subject { plsql.varchar2_api.is_nsv(nsv) }
       it { is_expected.to be true }
     end
     context 'when not passed nsv' do
-      subject { plsql.varchar2_api.is_nsv('nsv') }
+      let(:not_nsv) { 'not nsv' }
+      subject { plsql.varchar2_api.is_nsv(not_nsv) }
       it { is_expected.to be false }
     end
     context 'when passed null' do
