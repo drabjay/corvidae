1380521d53777ebd3fe57b783988d3e526d7d152
drabjay <johnbard@globalnet.co.uk>
2017-01-02 15:05:18 +0000

    varchar2_api functions is_nsv and nvl (#9)
    * varchar2_api.is_nsv function
    
    * Refactor tests
    
    * varchar2_api.nvl function

--- db/apps_data/packages/bodies/R__900_varchar2_utl.sql
+++ db/apps_data/packages/bodies/R__900_varchar2_utl.sql
@@ -15,5 +15,36 @@ BEGIN
   RETURN c_nsv;
 END nsv;
 
+/******************************************************************************\
+\******************************************************************************/
+FUNCTION is_nsv
+  (p_varchar2                     IN     VARCHAR2)
+RETURN BOOLEAN
+DETERMINISTIC
+IS
+BEGIN
+  IF p_varchar2 = c_nsv THEN
+    RETURN TRUE;
+  END IF;
+  RETURN FALSE;
+END is_nsv;
+
+/******************************************************************************\
+\******************************************************************************/
+FUNCTION nvl
+  (p_varchar2_1                   IN     VARCHAR2
+  ,p_varchar2_2                   IN     VARCHAR2)
+RETURN VARCHAR2
+DETERMINISTIC
+IS
+  l_varchar2                     t_maximum;
+BEGIN
+  l_varchar2 := p_varchar2_1;
+  IF is_nsv(p_varchar2_1) THEN
+    l_varchar2 := p_varchar2_2;
+  END IF;
+  RETURN l_varchar2;
+END nvl;
+
 END varchar2_utl;
 /
--- spec/packages/apis/varchar2_api_spec.rb
+++ spec/packages/apis/varchar2_api_spec.rb
@@ -1,8 +1,43 @@
 require 'spec_helper'
 
 describe 'varchar2_api' do
-  describe 'nsv' do
+  let(:nsv) { '$$$Null$$$' }
+
+  describe '.nsv' do
     subject { plsql.varchar2_api.nsv }
-    it { is_expected.to eq '$$$Null$$$' }
+    it { is_expected.to eq nsv }
+  end
+
+  describe '.is_nsv' do
+    context 'when passed nsv' do
+      subject { plsql.varchar2_api.is_nsv(nsv) }
+      it { is_expected.to be true }
+    end
+    context 'when not passed nsv' do
+      let(:not_nsv) { 'not nsv' }
+      subject { plsql.varchar2_api.is_nsv(not_nsv) }
+      it { is_expected.to be false }
+    end
+    context 'when passed null' do
+      subject { plsql.varchar2_api.is_nsv(NULL) }
+      it { is_expected.to be false }
+    end
+  end
+
+  describe '.nvl' do
+    let(:first)  { 'first' }
+    let(:second) { 'second' }
+    context 'when first argument is nsv' do
+      subject { plsql.varchar2_api.nvl(nsv, second) }
+      it { is_expected.to eq second }
+    end
+    context 'when first argument is not nsv' do
+      subject { plsql.varchar2_api.nvl(first, second) }
+      it { is_expected.to eq first }
+    end
+    context 'when first argument is null' do
+      subject { plsql.varchar2_api.nvl(NULL, second) }
+      it { is_expected.to be NULL }
+    end
   end
 end
