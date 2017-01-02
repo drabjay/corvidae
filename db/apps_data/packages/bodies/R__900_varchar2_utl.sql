ab5ce5795e24f639065e4361aa2bbe239539da66
John Bard <johnbard@globalnet.co.uk>
2017-01-02 14:59:03 +0000

    varchar2_api.nvl function

--- db/apps_data/packages/bodies/R__900_varchar2_utl.sql
+++ db/apps_data/packages/bodies/R__900_varchar2_utl.sql
@@ -29,5 +29,22 @@ BEGIN
   RETURN FALSE;
 END is_nsv;
 
+/******************************************************************************\
+\******************************************************************************/
+FUNCTION nvl
+  (p_varchar2_1                   IN     VARCHAR2
+  ,p_varchar2_2                   IN     VARCHAR2)
+RETURN VARCHAR2
+DETERMINISTIC
+IS
+  l_varchar2                     t_maximum;
+BEGIN
+  l_varchar2 := p_varchar2_1;
+  IF is_nsv(p_varchar2_1) THEN
+    l_varchar2 := p_varchar2_2;
+  END IF;
+  RETURN l_varchar2;
+END nvl;
+
 END varchar2_utl;
 /
--- spec/packages/apis/varchar2_api_spec.rb
+++ spec/packages/apis/varchar2_api_spec.rb
@@ -23,4 +23,21 @@ describe 'varchar2_api' do
       it { is_expected.to be false }
     end
   end
+
+  describe '.nvl' do
+    let(:first)  { 'first' }
+    let(:second) { 'second' }
+    context 'when first argument is nsv' do
+      subject { plsql.varchar2_api.nvl(nsv, second) }
+      it { is_expected.to eq second }
+    end
+    context 'when first argument is not nsv' do
+      subject { plsql.varchar2_api.nvl(first, second) }
+      it { is_expected.to eq first }
+    end
+    context 'when first argument is null' do
+      subject { plsql.varchar2_api.nvl(NULL, second) }
+      it { is_expected.to be NULL }
+    end
+  end
 end
