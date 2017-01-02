24de4216cd5eb40efe7081f57cae602a9d42bce0
John Bard <johnbard@globalnet.co.uk>
2017-01-02 21:27:25 +0000

    date_api functions is_nsv and nvl

--- db/apps_data/packages/bodies/R__900_date_utl.sql
+++ db/apps_data/packages/bodies/R__900_date_utl.sql
@@ -15,5 +15,36 @@ BEGIN
   RETURN c_nsv;
 END nsv;
 
+/******************************************************************************\
+\******************************************************************************/
+FUNCTION is_nsv
+  (p_date                         IN     DATE)
+RETURN BOOLEAN
+DETERMINISTIC
+IS
+BEGIN
+  IF p_date = c_nsv THEN
+    RETURN TRUE;
+  END IF;
+  RETURN FALSE;
+END is_nsv;
+
+/******************************************************************************\
+\******************************************************************************/
+FUNCTION nvl
+  (p_date_1                       IN     DATE
+  ,p_date_2                       IN     DATE)
+RETURN DATE
+DETERMINISTIC
+IS
+  l_date                         DATE;
+BEGIN
+  l_date := p_date_1;
+  IF is_nsv(p_date_1) THEN
+    l_date := p_date_2;
+  END IF;
+  RETURN l_date;
+END nvl;
+
 END date_utl;
 /
--- spec/packages/apis/date_api_spec.rb
+++ spec/packages/apis/date_api_spec.rb
@@ -7,4 +7,37 @@ describe 'date_api' do
     subject { plsql.date_api.nsv }
     it { is_expected.to eq nsv }
   end
+
+  describe '.is_nsv' do
+    context 'when passed nsv' do
+      subject { plsql.date_api.is_nsv(nsv) }
+      it { is_expected.to be true }
+    end
+    context 'when not passed nsv' do
+      let(:not_nsv) { Time.new(1001, 2, 3, 4, 5, 6) }
+      subject { plsql.date_api.is_nsv(not_nsv) }
+      it { is_expected.to be false }
+    end
+    context 'when passed null' do
+      subject { plsql.date_api.is_nsv(NULL) }
+      it { is_expected.to be false }
+    end
+  end
+
+  describe '.nvl' do
+    let(:first)  { Time.new(1001, 1, 1, 1, 1, 1) }
+    let(:second) { Time.new(2002, 2, 2, 2, 2, 2) }
+    context 'when first argument is nsv' do
+      subject { plsql.date_api.nvl(nsv, second) }
+      it { is_expected.to eq second }
+    end
+    context 'when first argument is not nsv' do
+      subject { plsql.date_api.nvl(first, second) }
+      it { is_expected.to eq first }
+    end
+    context 'when first argument is null' do
+      subject { plsql.date_api.nvl(NULL, second) }
+      it { is_expected.to be NULL }
+    end
+  end
 end
