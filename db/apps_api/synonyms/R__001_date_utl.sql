57bdf2674fcd018218b6578be664bf8ef035a244
John Bard <johnbard@globalnet.co.uk>
2017-01-02 21:04:19 +0000

    Initial date_api package

---
+++ db/apps_api/synonyms/R__001_date_utl.sql
@@ -0,0 +1,3 @@
+CREATE OR REPLACE SYNONYM date_utl FOR apps_data.date_utl;
+
+CREATE OR REPLACE SYNONYM date_api FOR apps_data.date_utl;
---
+++ db/apps_ui/synonyms/R__001_date_api.sql
@@ -0,0 +1,1 @@
+CREATE OR REPLACE SYNONYM date_api FOR apps_api.date_api;
